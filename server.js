76f8e9537550306ee4cfa1c29ef5950fc1d101bb
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-10-07 16:13:39 -0700

    Made a require function to get the routes file

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/html/htmlRoutes.js')(app);
+
