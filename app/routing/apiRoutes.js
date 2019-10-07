d6e3ff00dcc174755ebcb6c047b868482a1b6141
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-10-07 16:31:06 -0700

    Made an HTML POST request via apiRoutes.js file. Made a variable to hold the req.body and scores

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,4 +4,15 @@ module.exports = function(app){
     app.get('/api/friends', function(req, res){
         res.json(friends);
     })
+
+    app.post('/api/friends', function(req, res){
+        var information = req.body;
+        
+        console.log(information);
+        
+        var scores = information;
+
+        console.log(scores);
+
+    })
 }
