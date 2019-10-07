605842be5593564b90366e58045fb9bbdb7bd680
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-10-07 16:24:24 -0700

    Made a get html request for survey.html file

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,4 +6,9 @@ module.exports = function(app){
         res.sendFile(path.join(__dirname, '../public/home.html'));
     });
 
+    app.get('/survey', function(req,res){
+        res.sendFile(path.join(__dirname, '../public/survey..html'));
+
+    });
+
 }
