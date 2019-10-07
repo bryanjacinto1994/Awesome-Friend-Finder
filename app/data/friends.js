99fb152744869bdc3d00c13a40b193a84b1ffff7
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-10-07 16:17:51 -0700

    via friends.js, made a variable that holds an array of names,photos, and their scores

---
+++ app/data/friends.js
@@ -0,0 +1,7 @@
+var friends = [
+    {
+        name: 'Shin Chan',
+        photo: 'http://images6.fanpop.com/image/answers/3125000/3125450_1357750508567.64res_195_259.jpg',
+        scores: [1,1,1,1,1,1,1,1,1,1]
+    }
+];
