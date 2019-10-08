b680198d63a09ef835f4325399d11680a144a856
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-10-07 17:39:51 -0700

    Changed the other questions name and id to their specific numbers

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,7 +7,7 @@ module.exports = function(app){
     });
 
     app.get('/survey', function(req,res){
-        res.sendFile(path.join(__dirname, '../public/survey..html'));
+        res.sendFile(path.join(__dirname, '../public/survey.html'));
 
     });
 
