82dadbe7e60a5950b4b35fc9e7a9c7dd85aa1a05
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-10-07 18:58:22 -0700

    Added more names photos and scores in the friends.js array file

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,27 +2,27 @@ var friends = [
     {
         name: 'Shin Chan',
         photo: 'https://media0.giphy.com/media/xT9IgxEt22nmHa0jeg/source.gif',
-        scores: [1,1,1,1,1,1,1,1,1,1]
+        scores: [1,3,4,2,4,2,1,5,3,3]
     },
     {
         name: 'Arnold Shortman',
-        photo: '',
-        scores: []
+        photo: 'https://media.giphy.com/media/xT1R9Y1cmnniQDyL5K/giphy.gif',
+        scores: [5,4,3,4,5,3,4,5,5,3]
     },
     {
         name: 'Peter Griffin',
-        photo: '',
-        scores: []
+        photo: 'https://media1.giphy.com/media/WWd8wUHuwQg1y/source.gif',
+        scores: [1,3,2,1,2,3,2,1,2,1]
     },
     {
         name: 'Spongebob Squarepants',
-        photo: '',
-        scores: []
+        photo: 'http://giphygifs.s3.amazonaws.com/media/nDSlfqf0gn5g4/giphy.gif',
+        scores: [4,4,3,5,3,4,5,3,4,3]
     },
     {
         name: 'Homer Simpson',
-        photo: '',
-        scores: []
+        photo: 'https://media1.giphy.com/media/kEKcOWl8RMLde/giphy.gif',
+        scores: [3,4,3,2,3,4,3,2,3,4]
     },
 ];
 
